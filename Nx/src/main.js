dfc3fa09f069e3c7c837569421f495eb249f406d
Eugen Ostafi <eugen.ostafi@isa.utm.md>
2025-06-05 19:00:37 +0300

    :zap: cards animation

--- Nx/src/main.js
+++ Nx/src/main.js
@@ -70,6 +70,20 @@ document.addEventListener("DOMContentLoaded", function() {
     { threshold: 0.2 }
   );
   aboutParagraphs.forEach(p => observer.observe(p));
+
+  //scroll anim
+  const clientCards = document.querySelectorAll('.card');
+  const cardObserver = new IntersectionObserver(
+    entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+        }
+      });
+    },
+    { threshold: 0.2 }
+  );
+  clientCards.forEach(card => cardObserver.observe(card));
 });
 
 const hamMenu = document.querySelector(".ham-menu");
