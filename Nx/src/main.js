7be9964f357927e8a1bb73b4504aef28d3365fb2
Eugen Ostafi <eugen.ostafi@isa.utm.md>
2025-06-05 17:52:19 +0300

    :zap: popup contacts

--- Nx/src/main.js
+++ Nx/src/main.js
@@ -80,3 +80,16 @@ hamMenu.addEventListener("click", () => {
   hamMenu.classList.toggle("active");
   offScreenMenu.classList.toggle("active");
 });
+
+
+const openBtn = document.getElementById("openModal");
+const closeBtn = document.getElementById("closeModal");
+const modal = document.getElementById("modal");
+
+openBtn.addEventListener("click", () => {
+  modal.classList.add("open");
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.classList.remove("open");
+});
